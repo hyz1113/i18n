8f699004f9ce21bb4844e0ea1d3542d385f8acf1
amyzhang <amy.z@hytechc.com>
2024-11-10 17:37:28 +0800

    perf: 打包文件

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "rollup-plugin-node-resolve";
+// import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import {uglify} from "rollup-plugin-uglify";
+
+const paths = {
+    input: {
+        root: "./src/bin/cli.js",
+    },
+    output: {
+        root: "dist/",
+    },
+};
+
+const fileName = `fed-i18n-words.js`;
+
+export default {
+    input: `${paths.input.root}`,
+    output: {
+        file: `${paths.output.root}${fileName}`,
+        format: "esm",
+        name: "i18n-words",
+    },
+    plugins: [
+        resolve(),
+        // commonjs(),
+        babel({
+            exclude: "node_modules/**",
+            runtimeHelpers: true,
+        }),
+        uglify(),
+    ],
+};
